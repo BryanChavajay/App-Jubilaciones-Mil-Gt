82699509b522ee6da5cdb51a81e16ed565d30546
Donald <thomaschavajay20@gmail.com>
2022-10-19 00:27:22 -0600

    "Configuracion inicial del proyecto con login"

---
+++ src/pages/Home.jsx
@@ -0,0 +1,51 @@
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useCookies } from "react-cookie";
+import axios from "axios";
+import { toast, ToastContainer } from "react-toastify";
+
+//Nota: debe cambiarse la ruta para la petición al backend
+
+function Home() {
+  const navigate = useNavigate();
+  const [cookies, setCookie, removeCookie] = useCookies([]);
+  useEffect(() => {
+    const verifyUser = async () => {
+      if (!cookies.jwt) {
+        navigate("/login");
+      } else {
+        const { data } = await axios.post(
+          "http://localhost:5000",
+          {},
+          {
+            withCredentials: true,
+          }
+        );
+        if (!data.status) {
+          removeCookie("jwt");
+          navigate("/login");
+        } else
+          toast(`Hi ${data.user} 🦄`, {
+            theme: "dark",
+          });
+      }
+    };
+    verifyUser();
+  }, [cookies, navigate, removeCookie]);
+
+  const logOut = () => {
+    removeCookie("jwt");
+    navigate("/login");
+  };
+  return (
+    <>
+      <div className="private">
+        <h1>Bienvenido</h1>
+        <button onClick={logOut}>Cerar Sesion</button>
+      </div>
+      <ToastContainer />
+    </>
+  );
+}
+
+export default Home;
